27ab604dd220daffb7bc6da8e9a028abb20937f3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-12-13 11:15:38 +0100

    '1'

--- 1_waga-wzrost_regresja_liniowa.py
+++ 1_waga-wzrost_regresja_liniowa.py
@@ -19,15 +19,13 @@ print('Podstawowe statystyki')
 # print(df.describe())
 print(df.describe().round(2).T.to_string())    # T - zamiana wierszy z kolumnami
 
-plt.hist(df.query("Gender=='Male'").Weight, bins=30)
-plt.hist(df.query("Gender=='Female'").Weight, bins=30)
-plt.show()
-
-sns.histplot(df.query("Gender=='Male'").Weight)
-sns.histplot(df.query("Gender=='Female'").Weight)
-plt.show()
-
-del (df['nowa'])
+# plt.hist(df.query("Gender=='Male'").Weight, bins=30)
+# plt.hist(df.query("Gender=='Female'").Weight, bins=30)
+# plt.show()
+#
+# sns.histplot(df.query("Gender=='Male'").Weight)
+# sns.histplot(df.query("Gender=='Female'").Weight)
+# plt.show()
 
 df = pd.get_dummies(df)
 
@@ -35,4 +33,13 @@ del (df['Gender_Male'])
 df = df.rename(columns={'Gender_Female': 'Gender'})
 print(df)
 
-df.to_csv('waga_wzrost_po_edycji.csv')
+# df.to_csv('waga_wzrost_po_edycji.csv', index=False)
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(df[['Height', 'Gender']], df['Weight'])
+print(f'Wspolczynnik kierunkowy: {model.coef_}')
+print(f'wyraz wolny: {model.intercept_}')
+
+print(f'waga = wzrost * 1.06960294 + plec * -8.80805024 -102.52081454490131')
+print(model.predict([[192, 0], [192, 1], [160, 1], [100, 1]]))
