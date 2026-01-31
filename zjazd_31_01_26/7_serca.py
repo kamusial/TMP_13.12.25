aa47263778b25cbb503d895a1aa1279435a0834f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-01-31 14:26:30 +0100

    '1'

--- zjazd_31_01_26/7_serca.py
+++ zjazd_31_01_26/7_serca.py
@@ -10,3 +10,40 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('dane\\heart.csv', comment='#')
+
+X = df.iloc[:, :-1]
+y = df.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+print('\nLogistic Regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+
+print('\nKNN')
+model = KNeighborsClassifier(n_neighbors=50, weights='distance')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+
+print('\nDrzewo decyzyjne')
+model = DecisionTreeClassifier(max_depth=300, min_samples_split=2)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+print(pd.DataFrame(model.feature_importances_, X.columns))
+# granice decyzyjne
+# from mlxtend.plotting import plot_decision_regions
+# plot_decision_regions(X, y, model)
+# plt.show()
+
+print('\nSVC')
+# kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}
+model = SVC()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
