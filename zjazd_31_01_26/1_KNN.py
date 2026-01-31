66be040e98ddfe755274c8fe8fc84ac73dd55220
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-01-31 09:48:19 +0100

    '1'

--- zjazd_31_01_26/1_KNN.py
+++ zjazd_31_01_26/1_KNN.py
@@ -6,3 +6,23 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
+df = pd.read_csv('dane\\iris.csv')
+print(f'Describe: {df.describe()}')
+print(df)
+print(f"Ile czego: {df['class'].value_counts() }")
+
+species = {
+    'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2
+}
+df['class_value'] = df['class'].map(species)
+print(f'\nDane po przygotowaniu:\n{df.head().to_string()}')
+
+# moja probka, moj kwiat
+sample = [5.6, 3.2, 5.2, 1.45]
+
+# plt.scatter(df.sepallength, df.sepalwidth)
+# plt.show()
+
+plt.scatter(5.6, 3.2, c='r')  # czerwona kropka
+sns.scatterplot(data=df, x='sepallength', y='sepalwidth', hue='class')
+plt.show()
